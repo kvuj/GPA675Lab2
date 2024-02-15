2aa003452a244098ac2b5d4ca2d98333742eef39
Timothée Leclaire-Fournier <timotheel-f@protonmail.com>
2024-02-14 19:42:47 -0500

    Ajout Body

--- GPA675Lab2StartingProject/Body.cpp
+++ GPA675Lab2StartingProject/Body.cpp
@@ -1,1 +1,57 @@
 #include "Body.h"
+
+Body::~Body()
+{
+}
+
+bool Body::isEmtpy() const
+{
+	return false;
+}
+
+size_t Body::size() const
+{
+	return size_t();
+}
+
+QPoint Body::first() const
+{
+	return QPoint();
+}
+
+QPoint Body::last() const
+{
+	return QPoint();
+}
+
+void Body::addFirst(QPoint position)
+{
+}
+
+void Body::addLast(QPoint position)
+{
+}
+
+void Body::add(size_t index, QPoint position)
+{
+}
+
+void Body::removeFirst()
+{
+}
+
+void Body::removeLast()
+{
+}
+
+void Body::remove(size_t index)
+{
+}
+
+void Body::clear()
+{
+}
+
+void Body::swap(size_t index0, size_t index1)
+{
+}
--- GPA675Lab2StartingProject/Body.h
+++ GPA675Lab2StartingProject/Body.h
@@ -2,9 +2,34 @@
 #ifndef BODY_H
 #define BODY_H
 
+#include "BodyItem.h"
+
+#include <QPoint>
+
 class Body
 {
+private:
+	Body();
+	~Body();
 
+	bool isEmtpy() const;
+	size_t size() const;
+	QPoint first() const;
+	QPoint last() const;
+
+	void addFirst(QPoint position);
+	void addLast(QPoint position);
+
+	void add(size_t index, QPoint position);
+	void removeFirst();
+	void removeLast();
+	void remove(size_t index);
+	
+	void clear();
+	void swap(size_t index0, size_t index1);
+private:
+	size_t mSize;
+	BodyItem* mHead, *mTail;
 };
 
 #endif //BODY_H
