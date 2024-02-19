50b62ba464ee5c389c996aade33aa68dfdb716f9
Timothée Leclaire-Fournier <timotheel-f@protonmail.com>
2024-02-19 00:14:02 -0500

    Body: Ajout de l'API manquant

--- GPA675Lab2StartingProject/Body.h
+++ GPA675Lab2StartingProject/Body.h
@@ -3,6 +3,7 @@
 #define BODY_H
 
 #include <QPoint>
+#include <QPainter>
 
 
 /*
@@ -31,6 +32,10 @@ public:
 
 	void clear();
 	void swap(size_t index0, size_t index1);
+	void swap(Body& otherBody);
+	void rotate(int indexFrom);
+	bool isColliding(QPoint const& position);
+	void draw(QPainter& painter);
 
 private:
 	class BodyItem
